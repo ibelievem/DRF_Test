4a4e6d9f97b0ab3c96c7fad57e434a779c75eef7
xws <xws>
2019-08-24 19:03:01 +0800

    用户认证

--- drf_test2/api/views.py
+++ drf_test2/api/views.py
@@ -3,12 +3,29 @@ from django.shortcuts import render
 # Create your views here.
 
 from rest_framework.views import APIView
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponse
 from api import models
 
 import hashlib
 import time
 
+from rest_framework import exceptions
+
+
+ORDER_DICT={
+    1:{
+        "name":"张三",
+        "age":18,
+        "gender":"男",
+        "content":"这是张三的信息"
+    },
+    2: {
+        "name": "李四",
+        "age": 20,
+        "gender": "男",
+        "content": "这是李四的信息"
+    },
+}
 
 # MD5加密
 def md5(user):
@@ -20,6 +37,9 @@ def md5(user):
 
 
 class AuthView(APIView):
+    '''
+    用于用户登录认证
+    '''
 
     def post(self,requset,*args,**kwargs):
 
@@ -49,11 +69,55 @@ class AuthView(APIView):
         return JsonResponse(ret)
 
 
+class Authtication(object):
+    def authenticate(self,request):
+        token=request._request.GET.get("token")
+        # filter() 返回对象数据集， .first() 获取数据集中的第一个
+        # filter 无数据返回空，但是 get 无数据会报错
+        token_obj=models.UserToken.objects.filter(token=token).first()
+        if not token_obj:
+            raise exceptions.AuthenticationFailed("用户认证失败")
+
+        # 在rest framework 内部会将整个两个字段赋值给request，以供后续操作使用
+        return (token_obj.user,token_obj)
 
 
+    def authenticate_header(self,request):
+        pass
 
 
+class OrderView(APIView):
+    """
+    订单相关业务
+    """
 
+    authentication_classes = [Authtication,]
+
+    def get(self,request,*args,**kwargs):
+
+        # request.user 为 token_obj.user
+        # request.auth 为 token_obj
+
+        # print(request.user)
+        # print(request.auth)
+
+        ret={"code":1000,"msg":None}
+        try:
+            ret["data"]=ORDER_DICT
+
+        except Exception as e:
+            pass
+
+        return JsonResponse(ret)
+
+class UserInfoView(APIView):
+    """
+    用户中心
+    """
+    authentication_classes = [Authtication,]
+
+    def get(self,request,*args,**kwargs):
+        return HttpResponse("用户信息")
 
 
 
